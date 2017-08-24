c61936821731395804bbdb0858ea4c7c65aeb394
Cody Unger <codyunger@g.ucla.edu>
2017-08-24 16:19:56 -0700

    Rough version of matches (full stack), add components for Friends

---
+++ react-client/src/components/FriendsList.jsx
@@ -0,0 +1,12 @@
+import FriendsListItem from './FriendsListItem.jsx';
+import React from 'react';
+
+const FriendsPendingRequests = (props) => (
+    
+  <div>
+    {props.friends.map()}
+  </div>
+
+)
+
+export default FriendsPendingRequests
