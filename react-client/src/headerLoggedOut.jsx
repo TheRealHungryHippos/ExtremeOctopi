8002b625daed650ab9e8d81b107418e255a29fc8
Cody Unger <codyunger@g.ucla.edu>
2017-08-26 11:08:59 -0700

    clean up sign in page

--- react-client/src/headerLoggedOut.jsx
+++ react-client/src/headerLoggedOut.jsx
@@ -9,26 +9,13 @@ const toPathMessages = compile(MY_ROUTE_MESSAGES)
 
 const HeaderLoggedOut = () => (
 
-  <nav className="navbar navbar-toggleable-md navbar-light bg-faded fixed-top navbar-custom">
-    <button className="navbar-toggler navbar-toggler-right collapsed"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+  <nav className="navbar navbar-light bg-faded fixed-top navbar-custom">
+    
     <h1>
       <div>
         <Link to="/login" className="navbar-brand">FriendZone</Link>
       </div>
     </h1>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item"><Link to='/login' className="nav-link">Sign In</Link></li>
-      </ul>
-    </div>
   </nav>
 
 );
